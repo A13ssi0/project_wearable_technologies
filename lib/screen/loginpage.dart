ab14862c38b3280d36f978af3af5191afe0d1289
Alessio Palatella <alessio.palatella@gmail.com>
2022-05-14 14:49:27 +0200

    Added fitbitter and authorization

--- lib/screen/loginpage.dart
+++ lib/screen/loginpage.dart
@@ -1,20 +1,51 @@
+import 'package:fitbitter/fitbitter.dart';
 import 'package:flutter/material.dart';
+import 'package:project_wearable_technologies/utils/strings.dart';
 
 class Loginpage extends StatelessWidget {
   const Loginpage({Key? key}) : super(key: key);
 
   static const route = '/';
   static const routename = 'login';
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(Loginpage.routename),
       ),
-      body: const Center(
-        child: Text('Hello, world!'),
-      ),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () async {
+                String? userId = await FitbitConnector.authorize(
+                    context: context,
+                    clientID: Strings.fitbitClientID,
+                    clientSecret: Strings.fitbitClientSecret,
+                    redirectUri: Strings.fitbitRedirectUri,
+                    callbackUrlScheme: Strings.fitbitCallbackScheme);
+                FitbitActivityTimeseriesDataManager
+                    fitbitActivityTimeseriesDataManager =
+                    FitbitActivityTimeseriesDataManager(
+                  clientID: Strings.fitbitClientID,
+                  clientSecret: Strings.fitbitClientSecret,
+                  type: 'steps',
+                );
+
+                //Fetch data
+                final stepsData = await fitbitActivityTimeseriesDataManager
+                    .fetch(FitbitActivityTimeseriesAPIURL.dayWithResource(
+                  date: DateTime.now().subtract(const Duration(days: 1)),
+                  userID: userId,
+                  resource: fitbitActivityTimeseriesDataManager.type,
+                )) as List<FitbitActivityTimeseriesData>;
+
+                // Use them as you want
+                final snackBar = SnackBar(
+                    content: Text(
+                        'Yesterday you walked ${stepsData[0].value} steps!'));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: const Text('Enter'))),
     );
   } //build
 
